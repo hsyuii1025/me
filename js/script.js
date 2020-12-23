1e97c9e72f78b439eb3efb8de9c945d102320773
hsyuii1025 <73637571+hsyuii1025@users.noreply.github.com>
2020-12-23 20:43:58 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ $(document).ready(function(){
         }, 2000);
     });
 
-    $(".press").mouseenter(function(){
-        $(".learn").css("color","#170312").css("letter-spacing","15px")
-        $(".press").css("background-color","#41D3BD")
+    $(".btn").mouseenter(function(){
+        $(".btn_content").css("color","#170312").css("letter-spacing","15px")
+        $(".btn").css("background-color","#41D3BD")
     });
-    $(".press").mouseleave(function(){
-        $(".learn").css("color","#41D3BD").css("letter-spacing","5px")
-        $(".press").css("background-color","#170312")
+    $(".btn").mouseleave(function(){
+        $(".btn_content").css("color","#41D3BD").css("letter-spacing","5px")
+        $(".btn").css("background-color","#170312")
     });
 
 });
