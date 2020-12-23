a589ba86b84cc064cfaa68d2adbfd38267c5ef01
hsyuii1025 <73637571+hsyuii1025@users.noreply.github.com>
2020-12-23 23:02:28 +0800

    Update catogory.js

--- js/catogory.js
+++ js/catogory.js
@@ -1,43 +1,45 @@
 $(document).ready(function(){
-    /*lg*/
-    $(".lg").mouseenter(function(){
-        $(".content_lg").fadeIn(200)
-        $(".title_lg").css("color","#FFFFF2")
-        $(".lg").css("background-color","#170312").css("border","1px solid #41D3BD")
-    });    
-    $(".lg").mouseleave(function(){
-        $(".content_lg").fadeOut(600).css("transition-delay","s")
-        $(".title_lg").css("color","#170312")
-        $(".lg").css("background-color","#41D3BD")
-    });
-    /*mid*/
-    $(".mid").mouseenter(function(){
-        $(".content_mid").fadeTo(200,1)
-        $(".title_mid").css("color","#FFFFF2")
-        $(".mid").css("background-color","#170312").css("border","1px solid #FFFFF2")
-    });    
-    $(".mid").mouseleave(function(){
-        $(".content_mid").fadeTo(600,0).css("transition-delay","s")
-        $(".title_mid").css("color","#170312")
-        $(".mid").css("background-color","#FFFFF2")
-    });
-    /*sm*/
-    $(".sm").mouseenter(function(){
-        $(".content_sm").fadeTo(200,1)
-        $(".title_sm").css("color","#FFFFF2")
-        $(".sm").css("background-color","#170312").css("border","1px solid #FDAB4A")
-    });    
-    $(".sm").mouseleave(function(){
-        $(".content_sm").fadeTo(600,0).css("transition-delay","s")
-        $(".title_sm").css("color","#170312")
-        $(".sm").css("background-color","#FDAB4A")
-    });
-
-    /*go back*/
-    $(".arrow_icon").mouseenter(function(){
-        $(".hint").fadeTo(200,1)
-    });
-    $(".arrow_icon").mouseleave(function(){
-        $(".hint").fadeTo(600,0)
-    });
+    if($(window).width() > 500){
+        /*lg*/
+        $(".por").mouseenter(function(){
+            $(".content_por").fadeTo(200,1)
+            $(".title_por").css("color","#41D3BD")
+            $(".por").css("background-color","#170312").css("border","1px solid #41D3BD")
+        });    
+        $(".por").mouseleave(function(){
+            $(".content_por").fadeTo(600,0).css("transition-delay","s")
+            $(".title_por").css("color","#170312")
+            $(".por").css("background-color","#41D3BD")
+        });
+        /*mid*/
+        $(".res").mouseenter(function(){
+            $(".content_res").fadeTo(200,1)
+            $(".title_res").css("color","#FFFFF2")
+            $(".res").css("background-color","#170312").css("border","1px solid #FFFFF2")
+        });    
+        $(".res").mouseleave(function(){
+            $(".content_res").fadeTo(600,0).css("transition-delay","s")
+            $(".title_res").css("color","#170312")
+            $(".res").css("background-color","#FFFFF2")
+        });
+        /*sm*/
+        $(".soc").mouseenter(function(){
+            $(".content_soc").fadeTo(200,1)
+            $(".title_soc").css("color","#FDAB4A")
+            $(".soc").css("background-color","#170312").css("border","1px solid #FDAB4A")
+        });    
+        $(".soc").mouseleave(function(){
+            $(".content_soc").fadeTo(600,0).css("transition-delay","s")
+            $(".title_soc").css("color","#170312")
+            $(".soc").css("background-color","#FDAB4A")
+        });
+    
+        /*go back*/
+        $(".arrow_icon").mouseenter(function(){
+            $(".hint").fadeTo(200,1)
+        });
+        $(".arrow_icon").mouseleave(function(){
+            $(".hint").fadeTo(600,0)
+        });
+    }
 });
